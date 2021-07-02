ad4df7b2ceceec149c0c4a48d8235e55f28d8df2
Sebastia Ginard Julia <sginard@gmail.com>
2021-07-02 20:09:54 +0200

    Suma, Resta, Multiplica y modulo de a,b,c

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -7,7 +7,13 @@ int main(){
     a = 10;
     b = 20;
     c = a + b;
-    printf("a = %d ; b = %d ; c = %d\n",a,b,c);
+    printf("Suma : a = %d ; b = %d ; c = %d\n",a,b,c);
+    c = a - b;
+    printf("Resta : a = %d ; b = %d ; c = %d\n",a,b,c);
+    c = a * b;
+    printf("Multiplica : a = %d ; b = %d ; c = %d\n",a,b,c);
+    c = a % b;
+    printf("Modulo : a = %d ; b = %d ; c = %d\n",a,b,c);
    
     //printf("HolaMundo!\n\n");
     system("pause");
